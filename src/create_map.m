b77303c130a84127788ea0b35633f6f1ec70a4e9
dgd-rosa <dgd.rosa@gmail.com>
2021-04-19 22:11:13 +0100

    dummy interface

--- src/create_map.m
+++ src/create_map.m
@@ -26,7 +26,7 @@ clc
 point_not_valid = true;
 disp("Draw the first location point, and type the coordinates.");
 while(point_not_valid)
-    h1 = drawcrosshair;
+    %h1 = drawcrosshair;
     lat1 = input("Latitude:")
     long1 = input("Longitude:")
     
@@ -38,6 +38,102 @@ disp("Draw the first location point, and type the coordinates:");
 % input(prompt);
 
 
-%h = drawpolyline('Color','green');
+h = drawpolyline('Color','green');
 
 % h2 = drawpoint
+%%
+
+%get longitude/latitude
+menu = true;
+while(menu)
+    %define the roads first
+    
+    %define stops
+    
+    %define traffic lights
+    
+    menu = false
+end
+drawRoads();
+% define roads
+function [] = drawRoads()
+    roads = true;
+    counter = 0;
+    while(roads)
+        %first
+        if counter == 0
+            display("Draw a road");
+            counter = 1;
+            %Viegas function?!
+            continue;
+        end
+        draw = input("Insert 1 to draw more/ 0 to quit: ");
+        if draw
+            %viegas function?!
+        elseif draw == 0
+            break;
+        else
+            draw = input("Insert 1 to draw more/ 0 to quit: ");
+        end
+    end
+end
+
+function [] = drawStops()
+    stops = true;
+    counter = 0;
+    while(stops)
+        %first
+        if counter == 0
+            draw = input("Do you Want to Place a Stop Signal? 1 - yes / 0 - no");
+            counter = 1;
+            if draw
+                %put stops
+            elseif
+                break;
+            else
+                draw = input("Do you Want to Place a Stop Signal? 1 - yes / 0 - no");
+            end
+            %Viegas function?!
+            continue;
+        end
+        draw = input("Insert 1 to place more/ 0 to quit: ");
+        if draw
+            %viegas function?!
+        elseif draw == 0
+            break;
+        else
+            draw = input("Insert 1 to draw more/ 0 to quit: ");
+        end
+    end
+end
+
+function [] = drawLights()
+    lights = true;
+    counter = 0;
+    while(lights)
+        %first
+        if counter == 0
+            draw = input("Do you Want to Place a Stop Signal? 1 - yes / 0 - no");
+            counter = 1;
+            if draw
+                %put stops
+            elseif
+                break;
+            else
+                draw = input("Do you Want to Place a Stop Signal? 1 - yes / 0 - no");
+            end
+            %Viegas function?!
+            continue;
+        end
+        draw = input("Insert 1 to place more/ 0 to quit: ");
+        if draw
+            %viegas function?!
+        elseif draw == 0
+            break;
+        else
+            draw = input("Insert 1 to draw more/ 0 to quit: ");
+        end
+    end
+end
+
+    
