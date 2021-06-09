22eec58e7a7256914d0caf109780cd0744a7a2e7
Axartex <marcoornelas@tecnico.ulisboa.pt>
2021-06-09 18:40:03 +0100

    just to merge

--- src/TrackPredict.m
+++ src/TrackPredict.m
@@ -1,12 +1,12 @@
-function theta_var_5m = TrackPredict(thetat, dist, k_p)
+function theta_var_10m = TrackPredict(thetat, dist, k_p)
     n = 10/dist;%dist in meters
-    theta_var_5m = 0;
+    theta_var_10m = 0;
     for i = k_p:k_p+n
         if i > length(thetat)
             break;
         end
-        theta_var_5m = theta_var_5m + thetat(i);
+        theta_var_10m = theta_var_10m + thetat(i);
     end
-    theta_var_5m = theta_var_5m/(i-k_p);
-    theta_var_5m = theta_var_5m - thetat(k_p);
+    theta_var_10m = theta_var_10m/(i-k_p);
+    theta_var_10m = theta_var_10m - thetat(k_p);
 end
